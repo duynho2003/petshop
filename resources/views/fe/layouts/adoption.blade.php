377a244ebbac77410c5135c0e85a938e42cd9ba2
Tommy Nguyen <khenchaynguyen@gmail.com>
2023-06-25 15:47:03 +0700

    up layout adoption

--- resources/views/fe/layouts/adoption.blade.php
+++ resources/views/fe/layouts/adoption.blade.php
@@ -12,7 +12,7 @@
             </div>
         </div>
         <div class="row justify-content-center">
-            <div class="col-lg-4 col-md-6">
+            <!-- <div class="col-lg-4 col-md-6">
                 <div class="adoption-shop-item">
                     <div class="adoption-shop-thumb">
                         <img src="{{ asset ('/fe/img/product/adoption_shop_thumb01.jpg') }}" alt="">
@@ -40,8 +40,8 @@
                         </div>
                     </div>
                 </div>
-            </div>
-            <div class="col-lg-4 col-md-6">
+            </div> -->
+            <!-- <div class="col-lg-4 col-md-6">
                 <div class="adoption-shop-item">
                     <div class="adoption-shop-thumb">
                         <img src="{{ asset ('/fe/img/product/adoption_shop_thumb02.jpg') }}" alt="">
@@ -185,6 +185,43 @@
                         </div>
                     </div>
                 </div>
+            </div> -->
+
+            <div class="row justify-content-center">
+                @foreach ($prods as $product)
+                <div class="col-lg-4 col-md-6">
+                    <div class="adoption-shop-item">
+                        <div class="adoption-shop-thumb">
+                            <img src="{{ asset('/images/' . $product->image) }}" alt="" width="460px" height="160px">
+
+                            <a href="{{ Route ('out-adoption',['id' => $product->id]) }}" class="btn">Adoption <img src="{{ asset ('fe/img/icon/w_pawprint.png') }}" alt=""></a>
+                        </div>
+                        <div class="adoption-shop-content">
+                            <h4 class="title"><a href="shop-details.html">{{ $product->name }}</a></h4>
+                            <div class="adoption-meta">
+                                <ul>
+                                    <li><i class="fas fa-cog"></i><a href="#">Golden Retriever</a></li>
+                                    <li><i class="far fa-calendar-alt"></i> Cost: <span style="text-decoration: line-through;">{{ $product->normal_price }}</span></li>
+
+                                </ul>
+                            </div>
+                            <div class="adoption-rating">
+                                <ul>
+                                    <li class="rating">
+                                        <i class="fas fa-star"></i>
+                                        <i class="fas fa-star"></i>
+                                        <i class="fas fa-star"></i>
+                                        <i class="fas fa-star"></i>
+                                        <i class="fas fa-star"></i>
+                                    </li>
+                                    <li class="price">Total Price : <span>{{ $product->promotion_price }}</span></li>
+
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                @endforeach
             </div>
         </div>
     </div>
